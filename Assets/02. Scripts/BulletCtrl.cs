4ed8be6a799effe6d811a0e5739f99fd0c8f1e98
microsong <jhn06046@hotmail.com>
2019-06-17 14:47:12 +0900

    총알 모델 + Rigidbody 컴포넌트

---
+++ Assets/02. Scripts/BulletCtrl.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCtrl : MonoBehaviour
+{
+    //총알의 파괴력
+    public int damage = 20;
+    //총알 발사 속도
+    public float speed = 1000.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().AddForce(transform.forward * speed);
+        //아래 코드로 대체 가능
+        //GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
