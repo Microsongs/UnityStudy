9518c2cb19135f2420e5ffde8e9c80b6f94da7bf
microsong <jhn06046@hotmail.com>
2019-06-21 10:13:11 +0900

    총알 발사 로직

---
+++ Assets/02. Scripts/MyGizmo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyGizmo : MonoBehaviour
+{
+    //보일 구체의 색상과 반지름 설정
+    public Color _color = Color.yellow;
+    public float _radius = 0.1f;
+
+    private void OnDrawGizmos()
+    {
+        //가즈모 색상&구체 생성
+        Gizmos.color = _color;
+        Gizmos.DrawSphere(transform.position, _radius);
+    }
+}
